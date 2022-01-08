13d4b1c1b545d4cedb4394d4cb70ec364c0d6052
Max <mrgasser@ucsc.edu>
2022-01-08 00:55:14 -0800

    first commit for asgn1

---
+++ asgn1/myweb.c
@@ -0,0 +1,28 @@
+/*
+Design
+1. Create socket from program args
+    - check for socket formation errors
+2. form request
+3. send request over socket
+    - check for send errors
+4. receive response from server
+    - check for errors in reponse
+5. Write response to output file
+ 
+*/
+
+/* Section Notes
+
+*/
+
+#include <sys/socket.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]) {
+
+}
+
+
